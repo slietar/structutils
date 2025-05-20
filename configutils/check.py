0314c91cb3a247a6cea242d636b2d58c9bf0c3e7
Simon Liétar <simon@lietar.net>
2025-05-21 00:48:54 +0200

    Add schema path in check errors

--- configutils/check.py
+++ configutils/check.py
@@ -9,21 +9,21 @@ from typing import Union
 from typeutils.format import format_type
 
 
-def check(schema, /):
+def check(schema, /, *, _path: str = ''):
   if typing.get_origin(schema) is Union:
     match typing.get_args(schema):
       case ((other_type, types.NoneType) | (types.NoneType, other_type)):
-        check(other_type)
+        check(other_type, _path=_path)
 
   match schema:
     case builtins.float | builtins.int | builtins.str | types.NoneType:
       return
 
     case GenericAlias(__origin__=builtins.list):
-      check(schema.__args__[0])
+      check(schema.__args__[0], _path=f'{_path}[]')
 
     case UnionType(__args__=((other_type, types.NoneType) | (types.NoneType, other_type))):
-      check(other_type)
+      check(other_type, _path=_path)
 
     case _ if inspect.isclass(schema):
       signature = inspect.signature(schema)
@@ -31,15 +31,15 @@ def check(schema, /):
       for parameter in signature.parameters.values():
         if parameter.kind is Parameter.POSITIONAL_ONLY:
           if parameter.default is Parameter.empty:
-            raise ValueError(f'Positional-only parameter "{parameter.name}" of {format_type(schema)} is not supported')
+            raise ValueError(f'Positional-only parameter "{parameter.name}" of {format_type(schema)} at {_path or '<root>'} is not supported')
 
         if parameter.kind is Parameter.VAR_POSITIONAL:
           continue
 
         if parameter.annotation is Parameter.empty:
-          raise ValueError(f'Parameter "{parameter.name}" of {format_type(schema)} is missing a type annotation')
+          raise ValueError(f'Parameter "{parameter.name}" of {format_type(schema)} at {_path or '<root>'} is missing a type annotation')
 
-        check(parameter.annotation)
+        check(parameter.annotation, _path=f'{_path}.{parameter.name}')
 
     case _:
-      raise ValueError(f'Unsupported type {format_type(schema)}')
+      raise ValueError(f'Unsupported type {format_type(schema)} at {_path or '<root>'}')
