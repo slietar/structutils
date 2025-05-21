9dc828fc1c64acd2bb4c0eb1014c7c7be6842ff4
Simon Liétar <simon@lietar.net>
2025-05-21 19:00:03 +0200

    Fix object instantiation

--- configutils/instantiate.py
+++ configutils/instantiate.py
@@ -6,7 +6,7 @@ import typing
 from dataclasses import dataclass
 from inspect import Parameter
 from types import GenericAlias, UnionType
-from typing import Any, Optional, Protocol, Union, override
+from typing import Any, Literal, Optional, override
 
 from typeutils import format_type, infer_type
 
@@ -14,7 +14,7 @@ from .error import InstantiationError
 from .load import load
 
 
-def instantiate(schema, first_data, /, *datas) -> Any:
+def instantiate(schema, first_data, /, *datas, _partial: bool = False) -> Any:
   """
   Instantiate a structure from a type and JSON-serializable corresponding data.
 
@@ -23,15 +23,24 @@ def instantiate(schema, first_data, /, *datas) -> Any:
 
   datas = (first_data, *datas)
 
-  print(f'Instantiating \033[31m{format_type(schema)}\033[0m with ' + ' and '.join(f'\033[31m{data}\033[0m' for data in datas))
+  # print(f'Instantiating \033[31m{format_type(schema)}\033[0m with ' + ' and '.join(f'\033[31m{data!r}\033[0m' for data in datas) + (' (partial)' if _partial else ''))
 
-  if typing.get_origin(schema) is Union:
-    match typing.get_args(schema):
-      case ((other_type, types.NoneType) | (types.NoneType, other_type)):
-        if first_data is None:
-          return None
+  match typing.get_origin(schema):
+    case typing.Union:
+      match typing.get_args(schema):
+        case ((other_type, types.NoneType) | (types.NoneType, other_type)):
+          if first_data is None:
+            # TODO: We still need to check the other types (maybe allow those to be partial)
+            return None
 
-        return instantiate(other_type, *(data for data in datas if data is not None))
+          return instantiate(other_type, *(data for data in datas if data is not None))
+
+    case typing.Literal:
+      for data in datas:
+        if data not in typing.get_args(schema):
+          raise InstantiationError(f'Expected {format_type(schema)}, got {repr(data)}')
+
+      return first_data
 
 
   data_types = [type(data) for data in datas]
@@ -59,15 +68,6 @@ def instantiate(schema, first_data, /, *datas) -> Any:
       return [instantiate(schema.__args__[0], item) for item in first_data]
 
     case UnionType(__args__=((other_type, types.NoneType) | (types.NoneType, other_type))):
-      # Alternative
-      #
-      # nonnone_datas = list(itertools.takewhile(lambda data: data is not None, datas))
-      #
-      # if not nonnone_datas:
-      #   return None
-      #
-      # return instantiate(other_type, *nonnone_datas)
-
       if first_data is None:
         return None
 
@@ -115,19 +115,26 @@ def instantiate(schema, first_data, /, *datas) -> Any:
         explicit_targets.append(target)
 
 
-      first_target = explicit_targets[0] if explicit_targets[0] is not None else schema
-
       if explicit_targets[0] is not None:
+        first_target = explicit_targets[0]
         relevant_datas = [first_data]
         SchemaError = ValueError
+
+        for data in datas[1:]:
+          instantiate(schema, data, _partial=True)
       else:
+        first_target = schema
         relevant_datas = [data for data, target in zip(datas, explicit_targets) if target is None]
         SchemaError = InstantiationError
+
+        for data, target in zip(datas, explicit_targets):
+          if target is not None:
+            instantiate(target, data, _partial=True)
 
       signature = inspect.signature(first_target)
 
       arguments = dict[str, Any]()
-      has_var = False
+      var_parameter = None
 
       for parameter in signature.parameters.values():
         if parameter.kind is Parameter.POSITIONAL_ONLY:
@@ -139,38 +146,31 @@ def instantiate(schema, first_data, /, *datas) -> Any:
         if parameter.kind is Parameter.VAR_POSITIONAL:
           continue
 
-        if parameter.kind is Parameter.VAR_KEYWORD:
-          has_var = True
-          continue
-
         if parameter.annotation is Parameter.empty:
           raise SchemaError(f'Parameter "{parameter.name}" of {format_type(schema)} is missing a type annotation')
 
-        found_argument = False
+        if parameter.kind is Parameter.VAR_KEYWORD:
+          var_parameter = parameter
+          continue
 
-        for data in relevant_datas:
-          if parameter.name not in data:
-            continue
+        values = [data[parameter.name] for data in relevant_datas if parameter.name in data]
 
-          value = instantiate(parameter.annotation, data[parameter.name])
-
-          if not found_argument:
-            arguments[parameter.name] = value
-            found_argument = True
-
-        if (not found_argument) and (parameter.default is Parameter.empty):
+        if values:
+          arguments[parameter.name] = instantiate(parameter.annotation, *values)
+        elif (parameter.default is Parameter.empty) and (not _partial):
           raise SchemaError(f'Missing required argument "{parameter.name}" for {format_type(first_target)}')
 
-      for data in relevant_datas:
-        for name, value in data.items():
-          if (name in arguments) or (name == '_target_'):
-            continue
+      extra_argument_names = {name for data in relevant_datas for name in data.keys() if name != '_target_'} - set(arguments.keys())
 
-          if has_var:
-            arguments[name] = instantiate(signature.parameters[name].annotation, value)
-            continue
-
+      for name in extra_argument_names:
+        if var_parameter is not None:
+          values = [data[name] for data in relevant_datas if name in data]
+          arguments[name] = instantiate(var_parameter.annotation, *values)
+        else:
           raise InstantiationError(f'Unexpected argument "{name}" for {format_type(first_target)}')
+
+      if _partial:
+        return None
 
       try:
         return first_target(**arguments)
@@ -189,7 +189,7 @@ def assert_raises(exception_type: type[Exception], /):
     if not isinstance(e, exception_type):
       raise
   else:
-    raise AssertionError(f'Expected {exception_type.__name__} to be raised, but no exception was raised.')
+    raise AssertionError(f'Expected {exception_type.__name__} to be raised, but no exception was raised')
 
 
 @dataclass
@@ -213,6 +213,15 @@ class D:
   def __eq__(self, other):
     return isinstance(other, D) and (self.p == other.p)
 
+class E:
+  def __init__(self, a: int, **b: str):
+    self.a = a
+    self.b = b
+
+  @override
+  def __eq__(self, other):
+    return isinstance(other, E) and (self.a == other.a) and (self.b == other.b)
+
 assert instantiate(int, 3) == 3
 assert instantiate(list[int], [3, 4]) == [3, 4]
 assert instantiate(float, 3) == 3.0
@@ -227,6 +236,11 @@ assert instantiate(D, dict(p=6)) == D(p=6)
 assert instantiate(A, dict(x=5), dict(x=3, y='4')) == A(5, '4')
 assert instantiate(Optional[A], dict(x=5), None, dict(x=3, y='4')) == A(5, '4')
 assert instantiate(Optional[A], None, dict(x=3, y='4')) is None
+assert instantiate(Literal['a', 3], 'a') == 'a'
+assert instantiate(A, dict(_target_='B', x=3, y='4'), dict(x=5)) == B(x=3, y='4')
+assert instantiate(E, dict(a=3)) == E(a=3)
+assert instantiate(E, dict(a=3, x='1')) == E(a=3, x='1')
+assert instantiate(E, dict(a=3), dict(x='1')) == E(a=3, x='1')
 
 
 with assert_raises(InstantiationError):
@@ -244,11 +258,23 @@ with assert_raises(InstantiationError):
 with assert_raises(InstantiationError):
   instantiate(A, dict(x=3, y='4', z=5))
 
-# Maybe change to ValueError?
 with assert_raises(InstantiationError):
   instantiate(C, dict())
 
+with assert_raises(InstantiationError):
+  instantiate(Literal['a', 3], 'b')
 
-class E(Protocol):
-  x: int
-  y: str
+# with assert_raises(InstantiationError):
+#   instantiate(Optional[int], None, 'a')
+
+with assert_raises(InstantiationError):
+  instantiate(A, dict(_target_='B', x=3, y='4'), dict(x='5'))
+
+with assert_raises(InstantiationError):
+  instantiate(E, dict(a=3, x=1))
+
+with assert_raises(InstantiationError):
+  instantiate(E, dict(a=3), dict(x=1))
+
+with assert_raises(InstantiationError):
+  instantiate(E, dict(a=3), dict(x='1'), dict(x=1))
