28c581da3ecf088e2c5852c2447bf2e0bf06fdbd
Simon Liétar <simon@lietar.net>
2026-05-30 21:42:49 +0200

    Improve resolution on type aliases

--- src/structutils/types/resolve.py
+++ src/structutils/types/resolve.py
@@ -1,6 +1,12 @@
+import functools
+import operator
 import typing
 from dataclasses import dataclass
-from typing import Any, NewType, TypeAliasType
+from types import UnionType
+from typing import Any, NewType, TypeAliasType, TypeVar
+
+from ..config.error import SchemaError
+from .format import format_type
 
 
 @dataclass(frozen=True, slots=True)
@@ -8,7 +14,7 @@ class ResolvedType:
   annotations: tuple[Any, ...]
   value: Any
 
-def resolve(schema, /):
+def resolve(schema, /, *, allow_unspecialized: bool = False) -> ResolvedType:
   match typing.get_origin(schema):
     case typing.Annotated:
       resolved = resolve(typing.get_args(schema)[0])
@@ -18,22 +24,42 @@ def resolve(schema, /):
         value=resolved.value,
       )
 
-    # case typing.Union:
-    #   return resolve(
-    #     functools.reduce(operator.or_, typing.get_args(schema)),
-    #   )
-
     case TypeAliasType(__value__=value):
+      if isinstance(value, TypeVar):
+        type_var_index = schema.__type_params__.index(value)
+
+        return ResolvedType(
+          annotations=(),
+          value=typing.get_args(schema)[type_var_index],
+        )
+
       return resolve(value[*typing.get_args(schema)])
 
   match schema:
-    case TypeAliasType():
-      return resolve(schema.__value__)
+    case TypeAliasType(__value__=value):
+      if not schema.__type_params__:
+        return resolve(value)
+
+      if not allow_unspecialized:
+        raise SchemaError(f'Cannot resolve unspecialized type alias {format_type(schema)}')
+
+      if isinstance(value, TypeVar):
+        return ResolvedType(
+          annotations=(),
+          value=typing.Any,
+        )
+
+      return resolve(value[(typing.Any,) * len(schema.__type_params__)])
     case NewType():
       return resolve(schema.__supertype__)
-
-    # case TypeVar():
-    #   raise TypeError(f'TypeVar {schema} cannot be resolved')
+    case UnionType(__args__=args):
+      return ResolvedType(
+        annotations=(),
+        value=functools.reduce(
+          operator.or_,
+          (resolve(arg).value for arg in args),
+        ),
+      )
 
   return ResolvedType(
     annotations=(),
--- src/structutils/types/resolve_test.py
+++ src/structutils/types/resolve_test.py
@@ -1,12 +1,29 @@
-from typing import Annotated, NewType
+from typing import Annotated, Any, NewType
 
+from ..config.error import SchemaError
+from ..config.utils import assert_raises
 from .resolve import resolve
 
 
 type A = int
-B = NewType('B', A)
+type B[T] = T
+type C[T] = list[T]
+type D[T] = T | int
+type E[T, S] = T | S
+
+F = NewType('F', A)
 
 assert resolve(A).value is int
 assert resolve(Annotated[int, 'foo', 'bar']).value is int
 assert resolve(Annotated[A, 'foo', 'bar']).value is int
-assert resolve(B).value is int
+assert resolve(B, allow_unspecialized=True).value is Any
+assert resolve(B[int]).value is int
+assert resolve(C[int]).value == list[int]
+assert resolve(B[int]).value is int
+assert resolve(B[int] | B[str]).value == int | str
+assert resolve(D[str]).value == int | str
+assert resolve(E[int, str]).value == int | str
+assert resolve(F).value is int
+
+with assert_raises(SchemaError):
+    resolve(B, allow_unspecialized=False)
